a6d927968bfa763451e9422ca244f811c7bbfd7c
dibya-sit <dibya@supremeit.net>
2021-09-18 22:57:27 +0545

    Header and Button Components

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button @click="onClick()" text="Create an Account" color="green" />
+        <Button text="Create an Account" color="red" />
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+    },
+    components:{
+        Button
+    },
+    methods: {
+        onClick(){
+            console.log('click')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+    }
+</style>
